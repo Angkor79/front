4d67086cb4faf545e4cf772fcf9025cb03d4bdb3
Angkor79 <136211303+Angkor79@users.noreply.github.com>
2024-02-11 19:12:45 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import RegisterForm from "./layout/RegisterForm";
+;
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
